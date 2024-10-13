4266358ff23c76bf389f293cf1853edeaa7ad7d5
Emeka Chidoka <chidokaemeka@gmail.com>
2024-10-13 11:58:19 +0100

    added message for delete endpoint

--- products/views.py
+++ products/views.py
@@ -1,11 +1,21 @@
 # products/views.py
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Product
 from .serializers import ProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category', 'price']
+    filterset_fields = ["category", "price"]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform.destroy(instance)
+        return Response(
+            {"message": f"Product '{instance.name}' has been deleted successfully."},
+            status=status.HTTP_204_OK,
+        )
--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 # users/views.py
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import CustomUser
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
 
 class UserListCreateView(generics.ListCreateAPIView):
@@ -15,3 +16,11 @@ class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"message": f"User '{instance.username}' was deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
